3a780d070b560442d0254eef754a4ee41658e25b
DevangMhatre <devangmhatre396@gmail.com>
2026-04-01 22:51:41 +0530

    Solved Build Issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,21 +3,13 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
-import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import store from "./redux/store";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <PayPalScriptProvider
-    options={{
-      clientId: import.meta.env.VITE_PAYPAL_CLIENT_ID,
-      currency: "USD",
-      intent: "capture",
-    }}
-  >
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </PayPalScriptProvider>,
+
+  <Provider store={store}>
+    <App />
+  </Provider>,
   // </StrictMode>,
 );
